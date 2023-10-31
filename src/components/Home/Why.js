8dd4cc4d0dd1c38d377a964d23d4f3a1f141547b
Abhishek Shukla <abhishekshukla@Abhisheks-MacBook-Air-2.local>
2023-11-01 02:35:56 +0530

    Updated Border Colours

--- src/components/Home/Why.js
+++ src/components/Home/Why.js
@@ -4,8 +4,11 @@ import './Why.css'
 function Why() {
     return (
         <div class="why">
-            <h1 class="heading">Why Global Techify</h1>
-            <div class="card">
+        <div class="head1">
+        <h1 class="heading">Why Global Techify</h1>
+        </div>
+            
+            <div class="why-card">
                 <div class="card1">
                     <img src="https://www.pw.live/version14/assets/img/group-20360.svg" />
                     Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis.
