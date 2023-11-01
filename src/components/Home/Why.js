d679da6fe1560d4ce1a79ec87c2749d5f9d6cada
Abhishek Shukla <abhishekshukla@Abhisheks-MacBook-Air-2.local>
2023-11-01 19:39:52 +0530

    Made cards responsive

--- src/components/Home/Why.js
+++ src/components/Home/Why.js
@@ -9,19 +9,19 @@ function Why() {
         </div>
             
             <div class="why-card">
-                <div class="card1">
-                    <img src="https://www.pw.live/version14/assets/img/group-20360.svg" />
+                <div class="tf card1">
+                    <img class="image" src="https://www.pw.live/version14/assets/img/group-20360.svg" />
                     Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis.
                 </div>
-                <p class="card2">
+                <p class="tf card2">
                     <img src="https://www.pw.live/version14/assets/img/group-20360.svg" />
                     Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis.
                 </p>
-                <div class="card3">
+                <div class="tf card3">
                     <img src="https://www.pw.live/version14/assets/img/group-20360.svg" />
                     Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis.
                 </div>
-                <p class="card4">
+                <p class="tf card4">
                     <img src="https://www.pw.live/version14/assets/img/group-20360.svg" />
                     Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis.
                 </p>
